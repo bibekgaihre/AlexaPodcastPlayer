b9b5b9d889bc5d25fa091ca1458d37f41e0b6c8e
Bibek Gaihre <bibek.gaihre@rumsan.com>
2019-06-06 22:46:01 +0545

    project setup complete

---
+++ routes/api/index.js
@@ -0,0 +1,8 @@
+// const apiRouter = require("express").Router();
+// const podcastRouter = require("../../modules/podcast/podcast.routes.api");
+// const usersRouter = require("../../modules/user/user.routes.api");
+
+// apiRouter.use("/podcast", podcastRouter);
+// apiRouter.use("/user", usersRouter);
+
+// module.exports = apiRouter;
