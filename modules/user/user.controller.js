803699d7e951f68f9c3ec3997389fd367717724a
Bibek Gaihre <bibek.gaihre@rumsan.com>
2019-06-18 22:42:13 +0545

    login api created

--- modules/user/user.controller.js
+++ modules/user/user.controller.js
@@ -6,13 +6,25 @@ class Controller {
   async create(payload) {
     const email = await UserModel.findOne({ email: payload.email }).exec();
     if (email) {
-      return Promise.resolve("User Already Exists");
+      return Promise.resolve({ message: "User Already Exists" });
     }
     let salt = await bcrypt.genSalt(10);
     let hash = await bcrypt.hash(payload.password, salt);
     payload.password = hash;
     let data = await UserModel.create(payload);
     return data;
+  }
+  async find(payload) {
+    let user = await UserModel.findOne({ email: payload.email }).exec();
+    if (!user) {
+      return Promise.resolve({ message: "Email or Password is incorrect. Please try again" });
+    }
+    let chkPass = await bcrypt.compare(payload.password, user.password);
+    if (chkPass) {
+      return user;
+      // const token = jwt.sign();
+    }
+    return Promise.resolve({ message: "Email or Password is incorrect. Please try again" });
   }
 }
 
