b45af6be53b16541720b556a0aa7b57ac710606f
Bibek Gaihre <bibek.gaihre@rumsan.com>
2019-06-18 22:57:05 +0545

    class changed to objects in controller

--- modules/user/user.controller.js
+++ modules/user/user.controller.js
@@ -1,31 +1,27 @@
 const UserModel = require("./user.model");
-const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
-class Controller {
-  async create(payload) {
-    const email = await UserModel.findOne({ email: payload.email }).exec();
-    if (email) {
-      return Promise.resolve({ message: "User Already Exists" });
-    }
-    let salt = await bcrypt.genSalt(10);
-    let hash = await bcrypt.hash(payload.password, salt);
-    payload.password = hash;
-    let data = await UserModel.create(payload);
-    return data;
+const create = async payload => {
+  const email = await UserModel.findOne({ email: payload.email }).exec();
+  if (email) {
+    return Promise.resolve({ message: "User Already Exists" });
   }
-  async find(payload) {
-    let user = await UserModel.findOne({ email: payload.email }).exec();
-    if (!user) {
-      return Promise.resolve({ message: "Email or Password is incorrect. Please try again" });
-    }
-    let chkPass = await bcrypt.compare(payload.password, user.password);
-    if (chkPass) {
-      return user;
-      // const token = jwt.sign();
-    }
+  let salt = await bcrypt.genSalt(10);
+  let hash = await bcrypt.hash(payload.password, salt);
+  payload.password = hash;
+  let data = await UserModel.create(payload);
+  return data;
+};
+const find = async payload => {
+  let user = await UserModel.findOne({ email: payload.email }).exec();
+  if (!user) {
     return Promise.resolve({ message: "Email or Password is incorrect. Please try again" });
   }
-}
+  let chkPass = await bcrypt.compare(payload.password, user.password);
+  if (chkPass) {
+    return user;
+  }
+  return Promise.resolve({ message: "Email or Password is incorrect. Please try again" });
+};
 
-module.exports = new Controller();
+module.exports = { create, find };
